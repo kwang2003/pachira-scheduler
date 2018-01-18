cc7dee60cb514e23f44550294ff9e2885354b6fc
Kevin Wang <wangxuzheng@aliyun.com>
2018-01-18 22:32:17 +0800

    添加了任务执行历史查询功能

--- src/main/java/com/pachiraframework/scheduler/controller/JobController.java
+++ src/main/java/com/pachiraframework/scheduler/controller/JobController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pachiraframework.common.ExecuteResult;
 import com.pachiraframework.domain.Page;
 import com.pachiraframework.scheduler.dto.SearchJobCriteria;
+import com.pachiraframework.scheduler.dto.SearchJobHistoryCriteria;
 import com.pachiraframework.scheduler.entity.Job;
+import com.pachiraframework.scheduler.entity.JobHistory;
+import com.pachiraframework.scheduler.service.JobHistoryService;
 import com.pachiraframework.scheduler.service.JobService;
 import com.pachiraframework.web.controller.BaseController;
 
@@ -27,6 +30,8 @@ import io.swagger.annotations.ApiResponses;
 public class JobController extends BaseController {
 	@Autowired
 	private JobService jobService;
+	@Autowired
+	private JobHistoryService jobHistoryService;
 
 	@ApiOperation(value = "任务查询", notes = "根据条件，检索匹配的任务列表", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "分页的任务列表信息") })
@@ -34,6 +39,15 @@ public class JobController extends BaseController {
 	public ResponseEntity<ExecuteResult<Page<Job>>> search(SearchJobCriteria criteria) {
 		Page<Job> page = jobService.search(criteria);
 		ExecuteResult<Page<Job>> result = ExecuteResult.newSuccessResult(page);
+		return ResponseEntity.ok(result);
+	}
+	
+	@ApiOperation(value = "任务执行历史查询", notes = "根据条件，检索匹配的任务执行历史列表", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "分页的任务执行历史记录列表信息") })
+	@RequestMapping(path = "/history", method = { RequestMethod.GET })
+	public ResponseEntity<ExecuteResult<Page<JobHistory>>> search(SearchJobHistoryCriteria criteria) {
+		Page<JobHistory> page = jobHistoryService.search(criteria);
+		ExecuteResult<Page<JobHistory>> result = ExecuteResult.newSuccessResult(page);
 		return ResponseEntity.ok(result);
 	}
 }
